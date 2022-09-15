4b2cf7983a12037d14182b91f0c873022df2e85e
JulioSuarez <julio.suarez91@gmail.com>
2022-09-15 00:34:32 -0400

    production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        if($this->app->environment('production')){
+            \URL::forceSchema('https');
     }
 }
